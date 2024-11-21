afc125681599e9552f03f14948708bce0bb1cfa7
yeuubonn2k4 <161863346+yeuubonn2k4@users.noreply.github.com>
2024-11-22 00:17:23 +0700

    final fix

--- src/main/java/com/oop/ptit/group4/shoppingweb/constants/ErrorMessage.java
+++ src/main/java/com/oop/ptit/group4/shoppingweb/constants/ErrorMessage.java
@@ -1,22 +1,22 @@
 package com.oop.ptit.group4.shoppingweb.constants;
 
 public class ErrorMessage {
-    public static final String CAPTCHA_ERROR = "Fill captcha";
-    public static final String EMAIL_NOT_FOUND = "Email not found";
-    public static final String PASSWORDS_DO_NOT_MATCH = "Passwords do not match.";
-    public static final String EMAIL_IN_USE = "Email is already in use.";
-    public static final String INVALID_PASSWORD_CODE = "Password reset code is invalid!";
-    public static final String ACTIVATION_CODE_NOT_FOUND = "Activation code not found.";
-    public static final String ORDER_NOT_FOUND = "Order not found.";
-    public static final String MOBILE_PHONE_NOT_FOUND = "Mobile phone not found.";
-    public static final String USER_NOT_FOUND = "User not found.";
-    public static final String EMPTY_FIRST_NAME = "First name cannot be empty";
-    public static final String EMPTY_LAST_NAME = "Last name cannot be empty";
-    public static final String PASSWORD_CHARACTER_LENGTH = "The password must be between 6 and 16 characters long";
-    public static final String PASSWORD2_CHARACTER_LENGTH = "The password confirmation must be between 6 and 16 characters long";
-    public static final String INCORRECT_EMAIL = "Incorrect email";
-    public static final String EMAIL_CANNOT_BE_EMPTY = "Email cannot be empty";
-    public static final String FILL_IN_THE_INPUT_FIELD = "Fill in the input field";
-    public static final String EMPTY_PHONE_NUMBER = "Phone number cannot be empty";
-    public static final String EMPTY_POST_INDEX = "Post index must contain 5 digits";
+    public static final String CAPTCHA_ERROR = "Hãy hoàn thành captcha";
+    public static final String EMAIL_NOT_FOUND = "Email không được tìm thấy";
+    public static final String PASSWORDS_DO_NOT_MATCH = "Mật khẩu không khớp.";
+    public static final String EMAIL_IN_USE = "Email đã được sử dụng.";
+    public static final String INVALID_PASSWORD_CODE = "Mã đặt lại mật khẩu không hợp lệ!";
+    public static final String ACTIVATION_CODE_NOT_FOUND = "Không tìm thấy mã kích hoạt.";
+    public static final String ORDER_NOT_FOUND = "Không tìm thấy đơn hàng.";
+    public static final String MOBILE_PHONE_NOT_FOUND = "Không tìm thấy số điện thoại di động.";
+    public static final String USER_NOT_FOUND = "Không tìm thấy người dùng.";
+    public static final String EMPTY_FIRST_NAME = "Họ và tên đệm không được để trống";
+    public static final String EMPTY_LAST_NAME = "Tên không được để trống";
+    public static final String PASSWORD_CHARACTER_LENGTH = "Mật khẩu phải dài từ 6 đến 16 ký tự";
+    public static final String PASSWORD2_CHARACTER_LENGTH = "Xác nhận mật khẩu phải dài từ 6 đến 16 ký tự";
+    public static final String INCORRECT_EMAIL = "Email không hợp lệ";
+    public static final String EMAIL_CANNOT_BE_EMPTY = "Email không được để trống";
+    public static final String FILL_IN_THE_INPUT_FIELD = "Hãy điền vào ô nhập liệu";
+    public static final String EMPTY_PHONE_NUMBER = "Số điện thoại không được để trống";
+    public static final String EMPTY_POST_INDEX = "Mã bưu điện phải chứa 5 chữ số";
 }
--- src/main/java/com/oop/ptit/group4/shoppingweb/constants/SuccessMessage.java
+++ src/main/java/com/oop/ptit/group4/shoppingweb/constants/SuccessMessage.java
@@ -1,11 +1,12 @@
 package com.oop.ptit.group4.shoppingweb.constants;
 
 public class SuccessMessage {
-    public static final String MOBILE_PHONE_EDITED = "Mobile phone successfully edited!";
-    public static final String MOBILE_PHONE_ADDED = "Mobile phone successfully added!";
-    public static final String PASSWORD_CODE_SEND = "Reset password code is send to your E-mail";
-    public static final String PASSWORD_CHANGED = "Password successfully changed!";
-    public static final String ACTIVATION_CODE_SEND = "Activation code has been sent to your email";
-    public static final String USER_ACTIVATED = "User successfully activated.";
-    public static final String USER_UPDATED = "User successfully updated.";
+    public static final String MOBILE_PHONE_EDITED = "Số điện thoại đã được chỉnh sửa thành công!";
+    public static final String MOBILE_PHONE_ADDED = "Số điện thoại đã được thêm thành công!";
+    public static final String PASSWORD_CODE_SEND = "Mã đặt lại mật khẩu đã được gửi tới email của bạn";
+    public static final String PASSWORD_CHANGED = "Mật khẩu đã được thay đổi thành công!";
+    public static final String ACTIVATION_CODE_SEND = "Mã kích hoạt đã được gửi tới email của bạn";
+    public static final String USER_ACTIVATED = "Người dùng đã được kích hoạt thành công.";
+    public static final String USER_UPDATED = "Người dùng đã được cập nhật thành công.";
+
 }

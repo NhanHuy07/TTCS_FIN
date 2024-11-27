0d262755e7876e4486d1219d064010593dbf9db0
Khuat Huy Nhan <NhanKH.B22CN575@stu.ptit.edu.vn>
2024-11-27 21:34:15 +0700

    Done

--- src/main/resources/db/migration/V2__Add_users.sql
+++ src/main/resources/db/migration/V2__Add_users.sql
@@ -10,6 +10,49 @@ values(2, 'test123@test.com', 'Thanh', 'Nguyen Tien', 'Ha Noi', 'Ha Dong', '1234
 insert into users(id, email, first_name, last_name, city, address, phone_number, post_index, activation_code, active, password, password_reset_code)
 values(3, 'test456@test.com', 'Hong', 'Nguyen Thu', 'Ha Noi', 'Cau Giay', '1234567890', '1234567890', null, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', null);
 
+INSERT INTO users (id, email, first_name, last_name, city, address, phone_number, post_index, activation_code, active, password, password_reset_code)
+VALUES (4, 'user1@example.com', 'Duc', 'Hoang Le', 'Ha Noi', 'Ha Dong', '0123456781', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(5, 'user2@example.com', 'Linh', 'Nguyen Thuy', 'Ho Chi Minh', 'Quan 1', '0123456782', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(6, 'user3@example.com', 'Dung', 'Nguyen Khac', 'Ha Noi', 'Ha Dong', '0123456783', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+ (7, 'user4@example.com', 'Nam', 'Bui Van', 'Ho Chi Minh', 'Binh Thanh', '0123456784', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(8, 'user5@example.com', 'Ha', 'Nguyen Thi', 'Ho Chi Minh', 'Quan 7', '0123456785', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(9, 'user6@example.com', 'Tan', 'Nguyen Van', 'Ha Noi', 'Ha Dong', '0123456786', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(10, 'user7@example.com', 'Nguyen', 'Cao Viet', 'Ho Chi Minh', 'Quan 10', '0123456787', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(11, 'user8@example.com', 'Anh', 'Nguyen Quoc', 'Ha Noi', 'Cau Giay', '0123456788', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+ (12, 'user9@example.com', 'Nam', 'Nguyen Van', 'Ha Noi', 'Ha Dong', '0123456789', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+ (13, 'user10@example.com', 'Linh', 'Do Xuan Linh', 'Ho Chi Minh', 'Binh Phuoc', '0123456790', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(14, 'user11@example.com', 'Lam', 'Dao Si', 'Ha Noi', 'Long Bien', '0123456791', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+ (15, 'user12@example.com', 'Truong', 'Tuan Tu', 'Ha Noi', 'Hoang Mai', '0123456792', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(16, 'user13@example.com', 'Anh', 'Nguyen Tuan', 'Ho Chi Minh', 'Quan 2', '0123456793', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+ (17, 'user14@example.com', 'Dang', 'Tran Hai', 'Ha Noi', 'Long Bien', '0123456794', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(18, 'user15@example.com', 'Duong', 'Dang Thai', 'Ha Noi', 'Gia Lam', '0123456795', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(19, 'user16@example.com', 'Hien', 'Nguyen Thi', 'Ho Chi Minh', 'Binh Duong', '0123456796', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+ (20, 'user17@example.com', 'Hoang', 'Long Nguyen', 'Ho Chi Minh', 'Quan 3', '0123456797', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(21, 'user18@example.com', 'Thach', 'Pham Ngoc', 'Ha Noi', 'Cau Giay', '0123456798', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+(22, 'user19@example.com', 'Dung', 'Bui Van', 'Ha Noi', 'Nguyen Trai', '0123456799', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL),
+ (23, 'user20@example.com', 'Ho', 'Cam Xuc', 'Ho Chi Minh', 'Binh Thanh', '0123456800', '123456', NULL, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', NULL);
 insert into user_role (user_id, roles) values (1, 'ADMIN');
 insert into user_role (user_id, roles) values (2, 'USER');
 insert into user_role (user_id, roles) values (3, 'USER');
+INSERT INTO user_role (user_id, roles)
+VALUES
+(4, 'USER'),
+(5, 'USER'),
+(6, 'USER'),
+(7, 'USER'),
+(8, 'USER'),
+(9, 'USER'),
+(10, 'USER'),
+(11, 'USER'),
+(12, 'USER'),
+(13, 'USER'),
+(14, 'USER'),
+(15, 'USER'),
+(16, 'USER'),
+(17, 'USER'),
+(18, 'USER'),
+(19, 'USER'),
+(20, 'USER'),
+(21, 'USER'),
+(22, 'USER'),
+(23, 'USER');
